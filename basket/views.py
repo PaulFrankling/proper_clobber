820f034f21381438319ff9ccb13c380c1d882d2a
PaulFrankling <franky.p@hotmail.co.uk>
2021-07-27 22:19:23 +0000

    Added js to update quantity and remove item with page reload and added adjust function to views.py and urls.py so basket updated adjusted item properly

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -35,3 +35,27 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjust the quantity of a specific product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    basket = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            basket[item_id]['items_by_size'][size] = quantity
+        else:
+            del basket[item_id]['items_by_size'][size]
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+        else:
+            basket.pop[item_id]
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
