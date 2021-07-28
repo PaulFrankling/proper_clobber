3cf6c08e1dace1925eb1105bae89c10e9b816aa5
PaulFrankling <franky.p@hotmail.co.uk>
2021-07-28 22:08:36 +0000

    Created checkout app and fixed PEP8 issues in products/models.py file

--- products/models.py
+++ products/models.py
@@ -22,14 +22,15 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_clothing_sizing = models.BooleanField(default=False, null=True, blank=True)
-    has_footwear_sizing = models.BooleanField(default=False, null=True, blank=True)
+    has_clothing_sizing = models.BooleanField(
+        default=False, null=True, blank=True)
+    has_footwear_sizing = models.BooleanField(
+        default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
 
     def __str__(self):
         return self.name
