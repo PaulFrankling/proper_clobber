e9cbbfcd36810650693f046f18938360e6cceebb
PaulFrankling <franky.p@hotmail.co.uk>
2021-08-03 22:49:41 +0000

    Added stripe to checkout app views and added missing speech marks to settings.py

--- checkout/views.py
+++ checkout/views.py
@@ -7,7 +7,11 @@ from basket.contexts import basket_contents
 
 import stripe
 
+
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(
@@ -17,13 +21,23 @@ def checkout(request):
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe Public Key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JK9zxFb44BjYqLVdvvwyLOpCfTHMknzQUsLtiEb24Gvms3Yl10lrX3o8wTrR1LjiCd5Fs8vO7C7TepASK28pwG400ECSD0Q9K',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
