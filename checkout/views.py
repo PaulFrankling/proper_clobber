67e36f521dbfc72747c6f438e16c9ec22f6a77f6
PaulFrankling <franky.p@hotmail.co.uk>
2021-08-04 23:12:21 +0000

    Finished setting up webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -50,7 +50,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
+            order = order_form.save(commit=False)
             order = order_form.save()
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
             for item_id, item_data in basket.items():
                 try:
                     product = Product.objects.get(id=item_id)
