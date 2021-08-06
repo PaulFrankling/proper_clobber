5cb1c9fc381fc8a073fd3267c8359576e0d17c03
PaulFrankling <franky.p@hotmail.co.uk>
2021-08-06 10:25:44 +0000

    Updated forms, views with css and js

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if (!countrySelected) {
+    $('#id_default_country').css('color', '#7E7F83');
+};
+$('#id_default_country').change(function () {
+    countrySelected = $(this).val();
+    if (!countrySelected) {
+        $(this).css('color', '#7E7F83');
+    } else {
+        $(this).css('color', '#000000');
+    }
+});
