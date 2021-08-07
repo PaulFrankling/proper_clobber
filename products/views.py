5b5f165b837e2e49a8a82f8b76e79741ecba62e4
PaulFrankling <franky.p@hotmail.co.uk>
2021-08-07 21:37:02 +0000

    Finished add_product functionality and tested to here it works properly

--- products/views.py
+++ products/views.py
@@ -76,6 +76,18 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """Add a product to website store"""
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Added Product successfully!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. \
+                Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,11 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile successfully updated!')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed.\
+                Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
