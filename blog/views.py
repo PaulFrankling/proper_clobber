e7766d46780d96f6f9b1dd003d98f29a2134e2af
PaulFrankling <franky.p@hotmail.co.uk>
2021-08-24 22:03:36 +0000

    Added edit post functionality to urls and views

--- blog/views.py
+++ blog/views.py
@@ -50,3 +50,32 @@ def add_post(request):
     }
 
     return render(request, 'blog/add_post.html', context)
+
+
+@login_required
+def edit_post(request, post_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, \
+            only store owners are authorised to do that.')
+        return redirect(reverse('home'))
+
+    post = get_object_or_404(BlogPost, pk=post_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated Post Successfully!')
+            return redirect(reverse('blog_detail', args=[post.id]))
+        else:
+            messages.error(request, 'Failed to update post. \
+                Please ensure the form is valid.')
+    else:
+        form = BlogForm(instance=post)
+        messages.info(request, f'You are editing the post: {post.title}')
+
+    context = {
+        'form': form,
+        'post': post,
+    }
+
+    return render(request, 'blog/edit_post.html', context)
