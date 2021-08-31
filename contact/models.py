adbf9647b747242092fbe527fb3ffa16c0eddc53
PaulFrankling <franky.p@hotmail.co.uk>
2021-08-31 20:48:28 +0000

    Started process of creating contact form, added urls and models so far

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactMessages(models.Model):
+
+    class Meta:
+        verbose_name = 'Received Messages'
+
+    first_name = models.CharField(max_length=54, null=False, blank=False)
+    last_name = models.CharField(max_length=54, null=False, blank=False)
+    email = models.EmailField(max_length=54, null=False, blank=False)
+    subject = models.CharField(max_length=54, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+    date_sent = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
