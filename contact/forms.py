08ea6a1b8148c41ea9a8494d44cfc1aa49aa25f3
PaulFrankling <franky.p@hotmail.co.uk>
2021-09-01 12:23:17 +0000

    Added contact form and edited contact form styling and removed asteriks from labels as well as rounded the form input fields

--- contact/forms.py
+++ contact/forms.py
@@ -13,3 +13,9 @@ class ContactForm(forms.ModelForm):
             'subject',
             'message',
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0'
