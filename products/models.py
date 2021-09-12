4933aa42164ebb07908c511fe6afe6a52062ce0a
PaulFrankling <franky.p@hotmail.co.uk>
2021-09-12 16:41:47 +0000

    Fixed flake8 errors in product models.py file and migrated changes

--- products/models.py
+++ products/models.py
@@ -7,12 +7,13 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, default='', blank=True)
 
     def __str__(self):
         return self.name
 
     def get_friendly_name(self):
+        """ Returns friendly name """
         return self.friendly_name
 
 
@@ -20,7 +21,7 @@ class Product(models.Model):
     """ Product Model """
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, default='', blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_clothing_sizing = models.BooleanField(
@@ -30,7 +31,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, default='', blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
