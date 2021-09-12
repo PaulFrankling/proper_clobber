f182c73b43abbe3dff3a2f7d54b6a6da5d686f2c
PaulFrankling <franky.p@hotmail.co.uk>
2021-09-12 16:39:35 +0000

    Made changes to blog model and migrated to fix flake8 errors

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class BlogPost(models.Model):
     """ Blog post model """
     title = models.CharField(max_length=254, null=False, blank=False)
-    preview = models.TextField(max_length=254, null=True, blank=True)
-    article = models.TextField(null=True, blank=True)
-    author = models.CharField(max_length=54, null=True, blank=True)
+    preview = models.TextField(max_length=254, default='', blank=True)
+    article = models.TextField(default='', blank=True)
+    author = models.CharField(max_length=54, default='', blank=True)
     image = models.ImageField(null=True, blank=True)
-    blog_source = models.CharField(max_length=254, blank=True, null=True)
-    source_url = models.URLField(max_length=1024, null=True, blank=True)
+    blog_source = models.CharField(max_length=254, blank=True, default='')
+    source_url = models.URLField(max_length=1024, default='', blank=True)
     post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
