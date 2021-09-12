c8b8f50006be76f201fc641de938c83f19462845
PaulFrankling <franky.p@hotmail.co.uk>
2021-09-12 16:46:01 +0000

    Fixed flake8 error in checkout model page

--- checkout/models.py
+++ checkout/models.py
@@ -20,10 +20,10 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, default='', blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    county = models.CharField(max_length=80, default='', blank=True)
+    postcode = models.CharField(max_length=20, default='', blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
@@ -77,7 +77,7 @@ class OrderLineItem(models.Model):
         on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True)
+    product_size = models.CharField(max_length=2, default='', blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2,
