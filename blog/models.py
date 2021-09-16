d98e7d642994c5fe39b40b041cea36aaf34eb3c2
PaulFrankling <franky.p@hotmail.co.uk>
2021-09-16 11:02:04 +0000

    Changed blog models slightly so some form fields had to have something entered into them

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class BlogPost(models.Model):
     """ Blog post model """
     title = models.CharField(max_length=254, null=False, blank=False)
-    preview = models.TextField(max_length=254, default='', blank=True)
-    article = models.TextField(default='', blank=True)
-    author = models.CharField(max_length=54, default='', blank=True)
+    preview = models.TextField(max_length=254, null=False, blank=False)
+    article = models.TextField(null=False, blank=False)
+    author = models.CharField(max_length=54, null=False, blank=False)
     image = models.ImageField(null=True, blank=True)
     blog_source = models.CharField(max_length=254, blank=True, default='')
     source_url = models.URLField(max_length=1024, default='', blank=True)
